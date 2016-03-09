86eefcec505e3f3a303ebd922f9d3fac5ef41ffd
unknown <p623419@LADM_4CSHLX1.corp.heb.com>
2016-03-08 23:31:25 -0600

    First commit setting up a simple fragment base to build the app on.

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxActivity.java
@@ -0,0 +1,34 @@
+package com.bignerdranch.android.beatbox;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_beat_box);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//    }
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
